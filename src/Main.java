e583b39b0ad80eb1c13d810542bb25bee0a9be35
Sairku <kukrias@gmail.com>
2024-06-30 21:13:59 +0300

    add calculator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+        double result = sum(99.0, 1.0);
+        System.out.println(result);
+
+        double result1 = minus (44, 45);
+        System.out.println(result1);
+
+        double result2 = del(76,2);
+        System.out.println(result2);
+
+        double result3 = mno(43,43);
+        System.out.println(result3);
+
+    }
+    public static double sum( double x, double y){
+        double result = x + y;
+        return result;
+    }
+
+    public static double minus (double x, double y){
+        double result = x-y;
+        return result;
+    }
+    public static double del (double x, double y){
+        double result = x/y;
+        return result;
+    }
+    public static double mno(double x, double y){
+        double result = x*y;
+        return result;
     }
 }
